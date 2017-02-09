5c5ea2f0634cd324709f40248a194e22be232d61
unknown <williamlouisrigby@gmail.com>
2017-02-08 20:05:28 -0500

    first commit -- tutorial 1 completed

---
+++ Assets/BoardManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	private const float TILE_SIZE = 1.0f;
+	private const float TILE_OFFSET = 0.5f;
+
+	private int selectionX = -1;
+	private int selectionY = -1;
+
+    public List<GameObject> chessmanPrefabs;
+    private List<GameObject> activeChessman = new List<GameObject>();
+
+    private Quaternion orientation = Quaternion.Euler(0, 180, 0);
+
+    private void Start()
+    {
+        SpawnAllChessmans();
+    }
+
+    private void SpawnAllChessmans()
+    {
+        activeChessman = new List<GameObject>();
+
+        // spawn the white team
+
+        //king 
+        SpawnChessman(0, GetTileCenter(3, 0));
+
+        //queen
+        SpawnChessman(1, GetTileCenter(4, 0));
+
+        //rooks
+        SpawnChessman(2, GetTileCenter(0, 0));
+        SpawnChessman(2, GetTileCenter(7, 0));
+
+        //bishops
+        SpawnChessman(3, GetTileCenter(2, 0));
+        SpawnChessman(3, GetTileCenter(5, 0));
+
+        //knights
+        SpawnChessman(4, GetTileCenter(1, 0));
+        SpawnChessman(4, GetTileCenter(6, 0));
+
+        //pawns
+        for(int i = 0; i < 8; i++)
+        {
+            SpawnChessman(5, GetTileCenter(i, 1));
+        }
+
+        //spawn the black team
+
+        //king 
+        SpawnChessman(6, GetTileCenter(6, 7));
+
+        //queen
+        SpawnChessman(7, GetTileCenter(4, 7));
+
+        //rooks
+        SpawnChessman(8, GetTileCenter(0, 7));
+        SpawnChessman(8, GetTileCenter(7, 7));
+
+        //bishops
+        SpawnChessman(9, GetTileCenter(2, 7));
+        SpawnChessman(9, GetTileCenter(5, 7));
+
+        //knights
+        SpawnChessman(10, GetTileCenter(1, 7));
+        SpawnChessman(10, GetTileCenter(6, 7));
+
+        //pawns
+        for (int i = 0; i < 8; i++)
+        {
+            SpawnChessman(11, GetTileCenter(i, 6));
+        }
+    }
+	// Use this for initialization
+	private void DrawChessboard () 
+	{
+		Vector3 widthLine = Vector3.right * 8;
+		Vector3 heightLine = Vector3.forward * 8;
+
+		for (int i = 0; i <= 8; i++) 
+		{
+			Vector3 start = Vector3.forward * i;
+			Debug.DrawLine (start, start + widthLine);
+			for (int j = 0; j <= 8; j++) 
+			{
+                start = Vector3.right * j;
+                Debug.DrawLine(start, start + heightLine);
+            }
+		}
+
+        //draw the selection
+        if(selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(Vector3.forward * selectionY + Vector3.right * selectionX,
+                           Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1));
+
+            Debug.DrawLine(Vector3.forward * (selectionY + 1) + Vector3.right * selectionX,
+                           Vector3.forward * selectionY + Vector3.right * (selectionX + 1));
+        }
+	}
+
+    private void SpawnChessman(int index, Vector3 position)
+    {
+        GameObject go = Instantiate(chessmanPrefabs[index], position, orientation) as GameObject;
+        go.transform.SetParent(transform);
+        activeChessman.Add(go);
+    }
+	
+    private void UpdateSelection()
+    {
+        if(!Camera.main)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+    }
+
+    private Vector3 GetTileCenter(int x, int y)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += (TILE_SIZE * x) + TILE_OFFSET;
+        origin.z += (TILE_SIZE * y) + TILE_OFFSET;
+        return origin;
+    }
+
+	// Update is called once per frame
+	private void Update () 
+	{
+        UpdateSelection();
+        DrawChessboard ();
+	}
+}
